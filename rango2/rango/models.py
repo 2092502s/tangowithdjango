d212057e1b55a5432d824e2aa53c059bc593b88d
Lovisa Sundin <lovisafsundin@gmail.com>
2015-01-23 12:37:03 +0000

    Chapter 6 - completed

--- rango2/rango/models.py
+++ rango2/rango/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+#Basically, this defines a Category-class as a subclass of another
+#class called "Model" inside Django. The name-field basically inherits
+#the field inside Model that stores character data, and parameters are
+#specified.
+
+class Category(models.Model):
+    name = models.CharField( max_length=128, unique=True)
+    views = models.IntegerField(default=0)  #added afterwards
+    likes = models.IntegerField(default=0)  #added afterwards
+    
+    #Every category must be unique. 
+
+    def __unicode__(self):
+        return self.name
+    #An associated function that is conventional to define.
+    #Provides unicode representation.
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    #Basically, it references a Category object
+    title = models.CharField(max_length = 128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.title
+    #Not sure what it does.
--- rango2/rango/views.py
+++ rango2/rango/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
+#from rango.models import Category #CH7
 from django.http import HttpResponse
 
 def index(request):
 
+    #category_list = Category.objects.order_by('-likes')[:5] #CH7
     context_dict = { 'boldmessage': "I am bold font"}
+    #context_dict = {'categories': category_list} #CH7
 
     return render( request, 'rango/index.html', context_dict)
 
